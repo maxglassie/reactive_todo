22431649a305992399fa7b98f93cafabd0ad009e
Max Glassie <C-Max.Glassie@charter.com>
2019-01-16 12:54:30 -0700

    latest

---
+++ reactive_todo_backend/tests/routes/ekg.test.js
@@ -0,0 +1,2 @@
+const expect = require('chai').expect;
+const { ekg } = require('../../routes/ekg');
---
+++ reactive_todo_backend/tests/routes/users.test.js
@@ -0,0 +1,9 @@
+const expect = require('chai').expect;
+
+const { users } = require('../../routes/users');
+
+console.log(users)
+
+// setup
+
+//
