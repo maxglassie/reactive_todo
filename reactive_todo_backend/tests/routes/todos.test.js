c209e272c50e2d57b94d238ddb49a513ce28ec31
Michael Hughes <michael.r.hughes@accenture.com>
2019-01-18 13:57:13 -0600

    formatting

--- reactive_todo_backend/tests/routes/todos.test.js
+++ reactive_todo_backend/tests/routes/todos.test.js
@@ -6,21 +6,22 @@ const pry = require('pryjs')
 
 describe('Server', () => {
 
-  let todos = { 
-              todos: ["push to master", 
-                      "test in production"] 
-              }
+  let todos = {
+    todos: ["push to master",
+      "test in production"
+    ]
+  }
 
   describe('GET /todos', () => {
     it('should respond with todos', (done) => {
       request(app)
         .get('/todos')
         .expect(200)
-        .expect( (res) => {
+        .expect((res) => {
 
           expect(res.body).include(
             todos
-            );
+          );
         })
         .end(done)
     });
