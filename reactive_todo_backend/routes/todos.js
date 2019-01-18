9aba57ce269f175cc6605660dc532df81d2c6588
Michael Hughes <michael.r.hughes@accenture.com>
2019-01-18 17:18:35 -0600

    formatting

--- reactive_todo_backend/routes/todos.js
+++ reactive_todo_backend/routes/todos.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 // we update the object acts like a db
 // todos as a topic and Kafka stores the state?
 
-let todos_database = { 
-              todos: [ 
-                      // "push to master",
-                      // "test in prod" 
-                      ] 
-            }
+let todos_database = {
+  todos: [
+    // "push to master",
+    // "test in prod" 
+  ]
+}
 
 // clear(todos_database)
 // update(todos_database, "String")
@@ -25,15 +25,14 @@ module.exports = router;
 
 
 // feature tests - 5%
-  // as a user I go to the website and can see the thing
-  // I can click a thing and a thing happens
-  // system test
+// as a user I go to the website and can see the thing
+// I can click a thing and a thing happens
+// system test
 // integration tests - 10 - 20%
-  // anytime there's more than one function involved
-  // post route 
-  // multiple functions in one function
+// anytime there's more than one function involved
+// post route 
+// multiple functions in one function
 // unit tests - 60 - 80% 
-  // a single function 
-  // it makes a database call
-  // take one argument and returns one result
-
+// a single function 
+// it makes a database call
+// take one argument and returns one result
