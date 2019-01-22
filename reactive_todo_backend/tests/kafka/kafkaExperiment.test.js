6aae808d264d6d5dfd745dc3812c23d3972184fb
Michael Hughes <michael.r.hughes@accenture.com>
2019-01-22 11:36:39 -0600

    Autoformatting and added semi-colons

--- reactive_todo_backend/tests/kafka/kafkaExperiment.test.js
+++ reactive_todo_backend/tests/kafka/kafkaExperiment.test.js
@@ -1,13 +1,13 @@
 'use strict'
 
 const expect = require('chai').expect;
-const kafka  = require('no-kafka');
+const kafka = require('no-kafka');
 
 
 describe.only("kafka", () => {
   describe("no-kafka package ", () => {
     it("exists in repo", () => {
-      expect(typeof(kafka)).to.deep.equal("object")
+      expect(typeof (kafka)).to.deep.equal("object");
     });
   });
 
@@ -16,11 +16,11 @@ describe.only("kafka", () => {
       const producer = new kafka.Producer();
 
       // deeper dive into the producer object
-      console.log(producer)
+      console.log(producer);
 
-      expect(typeof(producer)).to.deep.equal("object")
+      expect(typeof (producer)).to.deep.equal("object");
       // default connection string is localhost
-      expect(producer.options.connectionString).to.deep.equal("kafka://127.0.0.1:9092")
+      expect(producer.options.connectionString).to.deep.equal("kafka://127.0.0.1:9092");
     });
   });
 });
